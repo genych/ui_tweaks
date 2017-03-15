5024d21a8dab0f2b22c3e9751f51f594ff3f8121
genych <e.shchekochikhin@dyninno.lv>
2017-03-15 09:01:42 +0200

    Change scope to chat; few more rules

--- wp.user.js
+++ wp.user.js
@@ -2,8 +2,8 @@
 // @name        wp
 // @namespace   esh
 // @description wp cleaner
-// @include     *.facebook.com*
-// @version     0.1
+// @include     *dyninno.facebook.com/chat*
+// @version     0.2
 // ==/UserScript==
 
 const removeScrolls = () => {document.body.style.overflow = "hidden";};
@@ -11,9 +11,12 @@ const removeScrolls = () => {document.body.style.overflow = "hidden";};
 const tweakCss = () =>
 {
   let rule = '';
+  // inline image
   rule += '.img {max-width: 80px !important; max-height: 80px !important}';
-  rule += '.__i_ {font-size: 12px !important}';
-  rule += '._hh7 {padding: 2px 4px !important}';
+  rule += '._52mr {max-width: 100% !important}'; // own message
+  rule += '.__i_ {font-size: 12px !important}'; // font size
+  rule += '._hh7 {padding: 2px 4px !important}'; // text density
+  rule += '._2poz {display: none !important}'; // inline link preview
 
   const css = document.createElement('style');
 
