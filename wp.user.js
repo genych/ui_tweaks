7bd02e776484ee867898507f102e0752d5e8fa8c
genych <e.shchekochikhin@dyninno.lv>
2017-05-02 12:57:14 +0300

    Fix images

--- wp.user.js
+++ wp.user.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // ==UserScript==
-// @name        wp
+// @name        wp_tweaks
 // @namespace   esh
-// @description wp cleaner
-// @include     *dyninno.facebook.com/chat*
+// @description clean and compact wp chat
+// @include     *.facebook.com/chat*
 // @grant none
-// @version     0.5
+// @version     0.6
 // ==/UserScript==
 
 const removeScrolls = () => {document.body.style.overflow = "hidden";};
@@ -16,6 +16,8 @@ const tweakCss = () =>
   let rule = '';
   // inline image
   rule += '.img {max-width: 80px !important; max-height: 80px !important}';
+  rule += '._4tsk {min-height: 80px !important}';
+
   rule += '._52mr {max-width: 100% !important}'; // own message
   rule += '.__i_ {font-size: 12px !important}'; // font size
   rule += '._hh7 {padding: 2px 4px !important}'; // text density
